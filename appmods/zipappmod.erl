33053fab6bea88000e730912685a0448639da4da
santiagomorales <santiaguete87@gmail.com>
2014-11-03 19:04:08 +0000

    Little communication between processes and browser.

--- appmods/zipappmod.erl
+++ appmods/zipappmod.erl
@@ -8,10 +8,23 @@ box(Str) ->
 	{'div', [{class, "box"}],
 		{pre, [], Str}}.
 
+prueba() ->
+	receive
+		A ->
+			io:format("Received:~p~n", [A#arg.appmoddata])
+	end,
+	prueba().
+
+
 out(A) ->
+	prueba ! A,
 	{ehtml,
 		[{p, [],
 			box(io_lib:format("A#arg.appmoddata = ~p~n",
 						 	  [A#arg.appmoddata]))
 	  }]}.
 
+
+
+start() ->
+	register(prueba, spawn(zipappmod, prueba, [])).
